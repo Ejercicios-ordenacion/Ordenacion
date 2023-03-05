9b49eed0fdb970a4ba2165a730b5633cd0b16278
luciacantos <luciacantos22@gmail.com>
2023-03-05 11:12:15 +0100

    Enunciado

---
+++ Ejercicio_2/Ordenación_topológica.py
@@ -0,0 +1,8 @@
+'''''
+Ejercicio 2: Ordenación topológica
+
+Una restricción se expresa mediante un par (i,j) de números enteros comprometidos entre 1 y n, que indica que la tarea ti va antes que la tarea tj. Es decir, la tarea ti debe estar
+terminada antes de empezar la tarea tj. La relación binaria <<...precede...>> así definida en el conjunto de las n tareas es una relación de orden parcial: algunas tareas no son comparables.
+- Hacer un algortimo que calcule una ordenación de las n tareas cumpliendo las restricciones.
+Está claro que no se pueden cumplir todas las restricciones. En este caso, no hay ordenación de las tareas. El algortimo deberá tratar este caso corretamente.
+'''''
