55d9f9b43275bb11076f667ee0f443e739e963b4
luciacantos <luciacantos22@gmail.com>
2023-03-05 21:57:51 +0100

    diccionario sucesores

--- Ejercicio_2/Ordenación_topológica.py
+++ Ejercicio_2/Ordenación_topológica.py
@@ -5,4 +5,12 @@ Una restricción se expresa mediante un par (i,j) de números enteros comprometi
 terminada antes de empezar la tarea tj. La relación binaria <<...precede...>> así definida en el conjunto de las n tareas es una relación de orden parcial: algunas tareas no son comparables.
 - Hacer un algortimo que calcule una ordenación de las n tareas cumpliendo las restricciones.
 Está claro que no se pueden cumplir todas las restricciones. En este caso, no hay ordenación de las tareas. El algortimo deberá tratar este caso corretamente.
+-----
+Entrada: n tareas y restricciones
+Salida: ordenación topológica de las tareas (si es quue es posible encontrar una ordenación)
 '''''
+
+def ord_topologica(n, restricciones):
+    # diccionario para ir almacenando
+    sucesor = {i: [] for i in range(1, n+1)}
+    
